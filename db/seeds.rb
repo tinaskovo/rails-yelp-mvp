bf3463118087dcea3c4df791ba2b52876d27f147
Tina Skov Olesen <tinaskovo@gmail.com>
2018-05-10 10:46:04 +0200

    cleaning tree

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian'
+  },
+  {
+    name:         'Ninos',
+    address:      'Guldbergsgade',
+    category:     'italian'
+  },
+  {
+    name:         'Banh Mi',
+    address:      'Elmegade',
+    category:     'japanese'
+  },
+  {
+    name:         'Torvets Kebab',
+    address:      'Skt Hans Torv',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
